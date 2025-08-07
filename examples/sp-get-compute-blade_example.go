a359b261ae6941129e844b01dcbd0bea40a60974
Gabriel Francisco Alves <gabriel.francisco@totvs.com.br>
2025-08-07 11:59:15 -0300

    refactor example to enable compute blade retrieval and improve error handling

--- examples/sp-get-compute-blade_example.go
+++ examples/sp-get-compute-blade_example.go
@@ -10,7 +10,7 @@ func main() {
 	endPoint := "https://ucsm01.example.com/"
 	username := "admin"
 	password := "secret"
-	//spDn := "org-root/org-TPSP1"
+	spDn := "org-root/org-TPSP1"
 	client, err := util.AaaLogin(endPoint, username, password)
 	//client.SetDebug(true)
 	if err != nil {
@@ -18,19 +18,19 @@ func main() {
 		return
 	}
 	defer client.AaaLogout()
-	//if computeBlade, err := util.SpGetComputeBlade(client, spDn); err != nil {
-	//	fmt.Printf("Error: %s\n", err)
-	//} else {
-	//	if computeBlade != nil {
-	//		fmt.Printf("Location: %s\n", computeBlade.Dn)
-	//		fmt.Printf("Model: %s\n", computeBlade.Model)
-	//		fmt.Printf("Number of CPUs: %d\n", computeBlade.NumOfCpus)
-	//		fmt.Printf("Number of Cores: %d\n", computeBlade.NumOfCores)
-	//		fmt.Printf("Total Memory: %d\n", computeBlade.TotalMemory)
-	//	} else {
-	//		fmt.Println("Error: no compute blade assigned")
-	//	}
-	//}
+	if computeBlade, err := util.SpGetComputeBlade(client, spDn); err != nil {
+		fmt.Printf("Error: %s\n", err)
+	} else {
+		if computeBlade != nil {
+			fmt.Printf("Location: %s\n", computeBlade.Dn)
+			fmt.Printf("Model: %s\n", computeBlade.Model)
+			fmt.Printf("Number of CPUs: %d\n", computeBlade.NumOfCpus)
+			fmt.Printf("Number of Cores: %d\n", computeBlade.NumOfCores)
+			fmt.Printf("Total Memory: %d\n", computeBlade.TotalMemory)
+		} else {
+			fmt.Println("Error: no compute blade assigned")
+		}
+	}
 	blades, err := util.GetAllComputeBlades(client)
 	if err != nil {
 		fmt.Printf("Error: %s\n", err)
