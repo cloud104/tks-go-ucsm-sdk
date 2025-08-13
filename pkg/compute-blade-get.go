922181567acd3da9e237976335204100d07cdca9
Gabriel Francisco Alves <gabriel.francisco@totvs.com.br>
2025-08-13 10:46:21 -0300

    fix: simplify return statement in compute-blade-get.go

--- pkg/compute-blade-get.go
+++ pkg/compute-blade-get.go
@@ -223,7 +223,7 @@ func ComputeBladeGetAvailable(c *api.Client, bladeSpec *BladeSpec) (computeBlade
 	}
 	if err = c.ConfigResolveClass(req, &out); err == nil {
 		computeBlades = &out.ComputeBlades
-		return &out.ComputeBlades, nil
+		return computeBlades, nil
 	}
 	return nil, fmt.Errorf("failed to resolve compute blades: %w", err)
 }
